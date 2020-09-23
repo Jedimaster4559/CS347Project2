92c5af866d466b266bbc50c1aedd0d205085c81b
Yingren Wang <46463357+yingren-wang@users.noreply.github.com>
2020-09-23 00:01:42 -0500

    player movement (#1)
    Co-authored-by: Yingren Wang <k.wang980711@gmmail.com>

---
+++ CS347Project2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // enum for facing
+    public enum Facing { LEFT, RIGHT };
+
+    // Hidden publics
+    [HideInInspector]
+    public Facing facing;
+
+    // Variables
+    public float moveSpeed = 1.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0).normalized * moveSpeed * Time.deltaTime);
+    }
+}
