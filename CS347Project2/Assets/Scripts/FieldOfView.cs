3a5290023b6f7f74082d892f6e39a5d6b7dc0ef7
Yingren Wang <k.wang980711@gmmail.com>
2020-10-06 13:34:58 -0500

    added script for viewcone

---
+++ CS347Project2/Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A Script to define field of view
+/// Attaching this script to a game object can give the object a viewcone.
+/// In this viewcone, players and enemies will not block the viewcone, but the walls will.
+/// 
+/// In order to have enemies and players not blocking the viewcone:
+///     This script requires all objects that are supposed to block the view to be in the same layer (This layer is set as "Walls" for right now)
+///         The layer needs to be set as the layer with all view blocking objects as the layerMask variable in the FieldOfView
+///     Layer for the view blocking objects needed to be added as a new layer
+///         This can be set through the Inscpecter >> Layer >> Add Layer...
+///     
+/// In order to have walls blocking the viewcone:
+///     Colliding objects have to have a BoxCollider2D
+/// 
+/// Variable(s)
+///     int ViewConeSize - set the size of the viewcone
+///     
+/// TODO: decide what layer to use for all enemies and players
+/// </summary>
+/// <author>Nathan Solomon</author>
+
+public class FieldOfView : MonoBehaviour
+{
+    [SerializeField] private LayerMask layerMask; // used for not colliding with enemies
+
+    // Hidden Components
+    private Mesh mesh;
+
+    // Public Elements
+    // TODO: decide what should be private and what should not
+    public float fov;// define the angle for field of view
+    public Vector3 origin;
+    public int rayCount; // the more rays we have the finer the view cone would be
+    public float currentAngle;
+    public float startingAngle;
+
+    public float viewConeSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        viewConeSize = 5.0f;
+        fov = 90f; // define the angle for field of view
+        origin = Vector3.zero;
+        rayCount = 50; // the more rays we have the finer the view cone would be
+        currentAngle = 0f;
+        // layerMask = LayerMask.GetMask("Character");
+        float angleIncrease = fov / rayCount;
+        
+        Vector3[] vertices = new Vector3[rayCount + 2]; // add two for ray zero and origin
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        vertices[0] = origin;
+
+        int vertexIndex = 1; // since index 0 will be the origin, start at 1
+        int triangleIndex = 0;
+        for (int i = 0; i <= rayCount; i++)
+        {
+            Vector3 vertex;
+            layerMask = LayerMask.GetMask("Wall");
+            RaycastHit2D raycastHit2D = Physics2D.Raycast(origin, GetVectorFromAngle(currentAngle), viewConeSize, layerMask);
+
+            // detect collision
+            if (raycastHit2D.collider == null)
+            {
+                // not hitting
+                vertex = origin + GetVectorFromAngle(currentAngle) * viewConeSize;
+            }
+            else
+            {
+                print("collideing");
+                // hit with something
+                vertex = raycastHit2D.point;
+            }
+
+            vertices[vertexIndex] = vertex;
+
+            if (i > 0)
+            {
+                // triangle info
+                triangles[triangleIndex + 0] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+
+                triangleIndex += 3;
+            }
+
+            vertexIndex++;
+
+            currentAngle -= angleIncrease;
+        }
+
+        // assign vertices and triangles
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+    }
+
+    ///// <summary>
+    ///// A function that will set the origin
+    ///// </summary>
+    public void SetOrigin(Vector3 origin)
+    {
+        this.origin = origin;
+    }
+
+    ///// <summary>
+    ///// A function that will set the aim direction
+    ///// </summary>
+    public void SetAimDirection(Vector3 aimDirection)
+    {
+        startingAngle = GetAngleFromVectorFloat(aimDirection) - fov / 2f;
+    }
+
+    ///// <summary>
+    ///// Helper function for setting aim direction
+    ///// A function that will return the angle of a vector
+    ///// </summary>
+    public static float GetAngleFromVectorFloat(Vector3 direction)
+    {
+        direction = direction.normalized;
+        float n = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        if (n < 0)
+        {
+            n += 360; // convert to positive
+        }
+        return n;
+    }
+
+    /// <summary>
+    /// Helper function for viewcone raycast
+    /// A function that will return the vector of an angle.
+    /// </summary>
+    public static Vector3 GetVectorFromAngle(float angle)
+    {
+        float angleRad = angle * (Mathf.PI / 180f);
+        return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+    }
+}
+
