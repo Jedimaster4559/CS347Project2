189e33b4903e3385ca46e75c776b714dbd6c21bc
Nathan Solomon <nbsolomon4559@gmail.com>
2020-10-11 02:40:30 -0500

    Initial Audio Engine Setup

---
+++ CS347Project2/Assets/Scripts/Audio/FadeConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Used to indicate fading values. This is used with music tracks to
+/// allow clips to have their fades handled properly when changing between
+/// different audio segments.
+/// 
+/// NOTE: a fade of 0 indicates instant transition between segments.
+/// </summary>
+[System.Serializable]
+public class FadeConfiguration
+{
+    public float introFadeIn = 0;
+    public float introFadeOut = 0;
+    public float loopFadeIn = 0;
+    public float loopFadeOut = 0;
+    public float closingFadeIn = 0;
+    public float closingFadeOut = 0;
+
+}
