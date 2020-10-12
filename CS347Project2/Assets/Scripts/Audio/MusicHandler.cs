efd1a9b264f464129f584a515a8ba74eb09a6e81
Nathan Solomon <nbsolomon4559@gmail.com>
2020-10-11 21:16:00 -0500

    Initial Music Implementation (#7)
    * adding music to repository
    
    * Initial Audio Engine Setup
    
    * Adding more documentation
    
    * adding singletone behavior to the music engine
    
    * transfering new audio files to dev machine
    
    * updated audio files to improve crossfade between intro and loop music
    
    * removing unneeded tags implementation
    
    * renaming FadeConfiguration to be more clear

---
+++ CS347Project2/Assets/Scripts/Audio/MusicHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This Class will long term handle the processing of multiple audio files.
+/// For now, this just makes sure that the audio engine never gets re-instantiated
+/// </summary>
+public class MusicHandler : MonoBehaviour
+{
+    private static MusicHandler instance = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // If a music handler already has been created, we need to
+        // destroy this one.
+        if(instance != null)
+        {
+            Destroy(this.gameObject);
+        } else
+        {
+            instance = this;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
