06ae391e7d689f8c971da25a30ccbae8a79bac0d
Yingren Wang <k.wang980711@gmmail.com>
2020-11-03 23:06:52 -0600

    initial changes made for shoot()

---
+++ CS347Project2/Assets/Scripts/BulletController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float speed;
+    public GameObject enemy;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 1.0f;
+        // enemy = GameObject.Find("Enemy");
+        // player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        // bullets get shot
+        var displacement = transform.right * speed;
+        var newPosition = transform.position - displacement;
+
+        transform.position = newPosition;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<BulletController>())
+        {
+            // bullets can touch other bullets
+        }
+        else if (collision.gameObject.GetComponent<Scout>())
+        {
+            // bullets can touch enemy
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.name != enemy.name)
+        {
+            Destroy(this);
+        }
+        else
+        {
+
+        }
+    }
+}
--- CS347Project2/Assets/Scripts/Scout.cs
+++ CS347Project2/Assets/Scripts/Scout.cs
@@ -10,12 +10,26 @@ public class Scout : MonoBehaviour
     private bool up = true;
     public float rate = 0.8f;
     public GameObject player;
+    public GameObject bulletPrefab;
     public float radius = 200;
+
+    public float bulletAngle;
+    public float shootingTime = 0.1f;
+
+    // control how often to shoot
+    private float projectilePeriod;
+    private float timeTillNextProjectile;
+
+
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = gameObject.GetComponent<Rigidbody2D>();
         rigidBody.SetRotation(top);
+
+        // set variables for shooting frequency
+        projectilePeriod = 1.0f;
+        timeTillNextProjectile = projectilePeriod;
     }
 
  
@@ -25,7 +39,7 @@ public class Scout : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        timeTillNextProjectile -= Time.deltaTime;
         var x = this.gameObject.GetComponent<Rigidbody2D>().position.x;
         var y = this.gameObject.GetComponent<Rigidbody2D>().position.y;
         var x2 = player.GetComponent<Rigidbody2D>().position.x;
@@ -34,7 +48,8 @@ public class Scout : MonoBehaviour
         Vector2 toVector = player.transform.position - transform.position;
         float angleToTarget = Vector2.SignedAngle(transform.right, toVector);
         float test = Vector2.Angle(transform.right, toVector);
-        if (radius>4 || test>45)
+
+        if (radius > 4 || test > 45)
         {
             if (up == true && rigidBody.rotation <= top)
                 up = false;
@@ -48,12 +63,47 @@ public class Scout : MonoBehaviour
         else
         {
             rigidBody.rotation += angleToTarget*rate/6;
-            shoot();
+            if (timeTillNextProjectile <= 0)
+            {
+                shoot();
+                timeTillNextProjectile = projectilePeriod;
+            }
         }
         
     }
-    void shoot() { 
-    
+
+    private void FixedUpdate()
+    {
+        // determine the angle of shooting the bullet
+        var rise = this.transform.position.y - player.transform.position.y;
+        var run = this.transform.position.x - player.transform.position.x;
+        bulletAngle = Mathf.Rad2Deg * Mathf.Atan(rise / run);
+    }
+
+    void shoot() {
+        //Determine directions using Euler angles
+        Vector3 eulerAngle1 = new Vector3(0, 0, bulletAngle);
+
+        //Convert to Quaternions for Unity use
+        var quaternion1 = Quaternion.Euler(eulerAngle1);
+
+        var bulletOffset = new Vector3(0.0f, 0.0f, 0.0f);
+        //Create bullets
+        var bullet = Instantiate(bulletPrefab, transform.position + bulletOffset, quaternion1);
+
+        // assign player and enemy game objects to the initiated bullet
+        var controller = bullet.GetComponent<BulletController>();
+        controller.player = player;
+        controller.enemy = this.gameObject;
+
+
+        // bullet.transform.LookAt(player.transform);
+
+        ////If bullets are fired facing left, need to mirror sprite
+        //if (facing == Facing.LEFT)
+        //{
+        //    bullet1.GetComponent<SpriteRenderer>().flipY = false;
+        //}
     }
 }
     
