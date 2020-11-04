42414639bbcfe4119a8d3b1f8e6685e96037c347
Ryan Braden <38021669+rabraden@users.noreply.github.com>
2020-11-04 08:30:09 -0600

    Finished Tutorial
    Tutorial that teaches controls, how to throw objects and how to kill enemy. Dissapears after player does it for the first time. I ended up "kind -of" being able to make it a prefab, I have no experience with how this stuff works in unity but I think I did it right, you should be able to drag and drop the components into whatever level we end up using- I did this bc I know kristian was making some new level stuff so I wanted to make sure all my work was usable in the event of some extreme last minute changes.

---
+++ CS347Project2/Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*This code handles getting input for the creation and deletion of
+ all the required tutorial UI popups and other
+ functions related to it */
+public class TutorialManager : MonoBehaviour
+{
+    // public GameObject[] tutorialMenus;
+    // public GameObject[] uiCanvases;
+    public GameObject tutorialObject;
+    public GameObject tutorialEnemy;
+
+    public bool FirstMovement = false;
+    public bool FirstThrow = false;
+    public bool FirstBlood = false;
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.W) ||
+            Input.GetKeyDown(KeyCode.A) ||
+            Input.GetKeyDown(KeyCode.S) ||
+            Input.GetKeyDown(KeyCode.D))
+        {
+            FirstMovement = true;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            FirstThrow = true;
+          
+            /*tried to raycast but it wouldn't work*/
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                if (hit.transform.name == tutorialObject.name)
+                {
+                    FirstThrow = true;
+                }
+            }
+        }
+
+        if (tutorialEnemy == null)
+        {
+            FirstBlood = true;
+        }
+    }
+}
+        
+    
+
+
